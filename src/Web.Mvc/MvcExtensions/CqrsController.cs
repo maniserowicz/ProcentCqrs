e44a956a0d8fe305a1a7c29a9d52b5b9e899b659
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2011-07-13 11:12:57 +0200

    added possibility to send domain commands to all custom controllers registered in Autofac

--- src/Web.Mvc/MvcExtensions/CqrsController.cs
+++ src/Web.Mvc/MvcExtensions/CqrsController.cs
@@ -1,9 +1,10 @@
 ﻿using System.Web.Mvc;
+using ProcentCqrs.Domain.Core;
 
 namespace ProcentCqrs.Web.Mvc.MvcExtensions
 {
     public abstract class CqrsController : Controller
     {
-
+        public ICommandSender _commandSender { get; set; }
     }
 }
