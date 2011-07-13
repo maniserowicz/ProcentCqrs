c1cf5db4e446eb99500cbb72b717e6cb605bd673
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2011-07-13 19:41:45 +0200

    added command and event for adding new training

---
+++ src/Domain.Core/Commands/Trainings/AddTrainingCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace ProcentCqrs.Domain.Core.Commands.Trainings
+{
+    public class AddTrainingCommand : ICommand
+    {
+        public readonly string Name;
+
+        public AddTrainingCommand(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ src/Domain.Core/Events/Trainings/TrainingWasAdded.cs
@@ -0,0 +1,14 @@
+﻿namespace ProcentCqrs.Domain.Core.Events.Trainings
+{
+    public class TrainingWasAdded : IEvent
+    {
+        public readonly int Id;
+        public readonly string Name;
+
+        public TrainingWasAdded(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
