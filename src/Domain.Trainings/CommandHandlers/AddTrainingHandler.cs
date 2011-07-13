d4b7fc9be795a6f380c9c995853253bb2deb7016
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2011-07-13 19:43:51 +0200

    added handler for AddTrainingCommand

---
+++ src/Domain.Trainings/CommandHandlers/AddTrainingHandler.cs
@@ -0,0 +1,36 @@
+﻿using NHibernate;
+using ProcentCqrs.Domain.Core;
+using ProcentCqrs.Domain.Core.Commands.Trainings;
+using ProcentCqrs.Domain.Core.Events.Trainings;
+using ProcentCqrs.Infrastructure.NHibernate;
+
+namespace ProcentCqrs.Domain.Trainings.CommandHandlers
+{
+    public class AddTrainingHandler : Handles<AddTrainingCommand>
+    {
+        private readonly ISession _session;
+        private readonly IEventPublisher _eventPublisher;
+
+        public AddTrainingHandler(ISession session, IEventPublisher eventPublisher)
+        {
+            _session = session;
+            _eventPublisher = eventPublisher;
+        }
+
+        public void Handle(AddTrainingCommand message)
+        {
+            int newTrainingId = 0;
+
+            _session.InTransaction(
+                () =>
+                    {
+                        var training = new Training(message.Name);
+
+                        newTrainingId = (int) _session.Save(training);
+                    }
+                );
+
+            _eventPublisher.Publish(new TrainingWasAdded(newTrainingId, message.Name));
+        }
+    }
+}
