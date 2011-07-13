154e863c37a0156b0121f1a788bb48e3cdeddae4
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2011-07-13 13:55:07 +0200

    added view for trainings displayed in trainings index

---
+++ src/ProcentCqrsDb/Schema Objects/Schemas/readmodel/Views/readmodel.TrainingsForTrainingsIndex.view.sql
@@ -0,0 +1,2 @@
+﻿CREATE VIEW [readmodel].[TrainingsForTrainingsIndex]
+	AS SELECT t.Id, t.Name, (SELECT COUNT(*) FROM [dbo].[UserTrainings] ut WHERE ut.TrainingId=t.Id) AS UsersCount FROM [dbo].[Trainings] t
