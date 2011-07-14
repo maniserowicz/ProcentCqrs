ace7f2243a03fa71831c4a4c4b1510afe8e21a7e
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2011-07-14 11:47:24 +0200

    added javascript UI routines

---
+++ src/Web.Mvc/Scripts/app/app-ui.js
@@ -0,0 +1,18 @@
+﻿var ui = {
+    defaults: {
+        modalDialog: {
+            autoOpen: false,
+            resizable: false,
+            draggable: false,
+            modal: true,
+            width: 455
+        }
+    }
+    , init: function (ctx) {
+        $('.modal-dialog', ctx).dialog(ui.defaults.modalDialog);
+    }
+};
+
+$(function () {
+    ui.init($(document));
+})
