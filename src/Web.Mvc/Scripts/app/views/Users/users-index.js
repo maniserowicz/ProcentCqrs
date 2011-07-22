111331e957c1063c740e9b2386603b8e2289146c
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2011-07-22 08:16:36 +0200

    using Knockout.js for databindings related to rename operation

--- src/Web.Mvc/Scripts/app/views/Users/users-index.js
+++ src/Web.Mvc/Scripts/app/views/Users/users-index.js
@@ -3,37 +3,84 @@
 cqrs.v = {
     onRenamed: function () {
     }
+    , beforeRename: function () {
+        // necessary for Opera - it does not update underlying bindings
+        // when ENTER is pressed to issue form submission
+
+        // investigate 'valueUpdate' param to value binding
+        // first tests show that it causes exception when combining MVC helpers with jquery template
+        // and even simplest usage (outside of template) of 'valueUpdate' did not work as expected
+        // (http://knockoutjs.com/documentation/value-binding.html)
+        $('form input').blur();
+    }
 };
 
 $(function () {
     var dlgRenameUser = $('#dlgRenameUser');
-    var dlgRenameUserTitleTemplate = dlgRenameUser.data('title-template');
 
-    $('[data-command=rename-user]').click(function (e) {
-        e.preventDefault();
+    function User(data) {
+        this.Id = data.Id;
+        this.Email = ko.observable(data.Email);
+        this.FirstName = ko.observable(data.FirstName);
+        this.LastName = ko.observable(data.LastName);
 
-        var $row = $(this).closest('tr');
+        this.fullName = function () {
+            return cqrs.su.format("{0} {1}", this.FirstName(), this.LastName());
+        };
+    };
 
-        var userId = $row.data('id');
-        var firstName = $('[data-role=first-name]', $row);
-        var lastName = $('[data-role=last-name]', $row);
+    var viewModel = {
+        userIdBeingRenamed: ko.observable()
+        , users: ko.observableArray()
+    };
 
-        var newFirstName = $('#FirstName', dlgRenameUser);
-        var newLastName = $('#LastName', dlgRenameUser);
-
-        $('#UserId', dlgRenameUser).val(userId);
-        newFirstName.val(firstName.text());
-        newLastName.val(lastName.text());
+    viewModel.requestRename = function (id) {
+        this.userIdBeingRenamed(id);
+        var self = this;
 
         cqrs.v.onRenamed = function () {
-            dlgRenameUser.dialog('close');
+          dlgRenameUser.dialog('close');
 
-            firstName.text(newFirstName.val());
-            lastName.text(newLastName.val());
+            var renaming = self.renaming();
+            var user = self.findUser(renaming.Id);
+            user.FirstName(renaming.FirstName());
+            user.LastName(renaming.LastName());
+
+          self.userIdBeingRenamed(null);
         };
 
-        var newTitle = cqrs.su.format(dlgRenameUserTitleTemplate, firstName.text(), lastName.text());
-        dlgRenameUser.dialog('option', { title: newTitle });
         dlgRenameUser.dialog('open');
+    };
+
+    viewModel.findUser = function (id) {
+        return ko.utils.arrayFirst(this.users(), function (item) {
+            return item.Id == id;
+        });
+    };
+
+    viewModel.renaming = ko.dependentObservable(function () {
+        var id = this.userIdBeingRenamed();
+
+        if (!this.currentRename || this.currentRename.Id || this.currentRename.Id != id) {
+            var user = this.findUser(id);
+            this.currentRename = new User(ko.toJS(user) || {});
+        }
+
+        return this.currentRename;
+    }, viewModel);
+
+    viewModel.renaming.subscribe(function (newValue) {
+        dlgRenameUser.dialog('option', 'title', cqrs.su.format('Rename ' + (newValue || {}).fullName()));
     });
-})
+
+    window.viewModel = viewModel;
+
+    for (var i = 0, l = cqrs.allUsers.length; i < l; i++) {
+        viewModel.users.push(new User(cqrs.allUsers[i], viewModel));
+    }
+
+    ko.applyBindings(viewModel);
+
+    // need to re-parse form client-side validation because it's in template
+    cqrs.vu.parse($('form', dlgRenameUser)[0]);
+});
