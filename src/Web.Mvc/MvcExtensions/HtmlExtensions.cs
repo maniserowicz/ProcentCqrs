9215aaff87436f54cf350c1fba67bc1234be46af
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2011-07-22 08:14:08 +0200

    added new extensions for @Html to generate ids and names of objects

---
+++ src/Web.Mvc/MvcExtensions/HtmlExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace System.Web.Mvc
+{
+    public static class HtmlExtensions
+    {
+        public static string NameFor<TModel, TProperty>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression)
+        {
+            return htmlHelper.ViewContext.ViewData.TemplateInfo.GetFullHtmlFieldName(ExpressionHelper.GetExpressionText(expression));
+        }
+
+        public static string IdFor<TModel, TProperty>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression)
+        {
+            return HtmlHelper.GenerateIdFromName(NameFor(htmlHelper, expression));
+        }
+    }
+}
