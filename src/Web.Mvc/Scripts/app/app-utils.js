32e41e95520b8ec1ad10222c120d0879609d68c4
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2011-07-22 08:16:03 +0200

    added validation client-side helper for re-parsing form's validation

--- src/Web.Mvc/Scripts/app/app-utils.js
+++ src/Web.Mvc/Scripts/app/app-utils.js
@@ -11,7 +11,18 @@ AppUtils.prototype = {
             return template;
         }
     }
+    , validation: {
+        parse: function (form) {
+            // first: remove existing validation for this form
+            // reference: http://blog.gutek.pl/post/2011/05/19/Edycja-zagniezdzonych-list-w-MVC-3-(Czesc-3).aspx
+            $.removeData(form, 'validator');
+            // then: recreate validation for this form
+            // reference: http://www.maciejaniserowicz.com/post/2011/03/23/Odswiezenie-walidacji-client-side-w-MVC3.aspx
+            $.validator.unobtrusive.parse(form);
+        }
+    }
 };
 
 cqrs.utils = new AppUtils();
 cqrs.su = cqrs.utils.string;
+cqrs.vu = cqrs.utils.validation;
