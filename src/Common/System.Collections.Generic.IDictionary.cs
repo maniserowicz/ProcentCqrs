b4511cb6c85ae63f8a924838dfa84a74cc5a819e
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2011-07-22 08:13:41 +0200

    added helpers for dictionary

---
+++ src/Common/System.Collections.Generic.IDictionary.cs
@@ -0,0 +1,19 @@
+﻿namespace System.Collections.Generic
+{
+    public static class DictionaryExtensions
+    {
+        public static TValue ValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> @this, TKey key)
+        {
+            TValue result = default(TValue);
+
+            if (@this == null)
+            {
+                return result;
+            }
+
+            @this.TryGetValue(key, out result);
+
+            return result;
+        }
+    }
+}
