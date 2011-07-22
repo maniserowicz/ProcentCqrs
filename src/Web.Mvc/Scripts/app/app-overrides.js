dc6a8d4dfc7c8f31d9a13e22a1f467060cbbe917
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2011-07-22 12:51:36 +0200

    closed ko.js override in a function to avoid polluting global namespace

--- src/Web.Mvc/Scripts/app/app-overrides.js
+++ src/Web.Mvc/Scripts/app/app-overrides.js
@@ -2,13 +2,14 @@
 
 /* override default knockout.js value binding handler
 to update value on 'afterkeypress' by default instead of 'change' only */
+(function() {
 var valueBindingHandler = ko.bindingHandlers['value'];
 var originalInit = valueBindingHandler.init;
 
 valueBindingHandler.init = function (element, valueAccessor, allBindingsAccessor) {
     var oldValueUpdate = allBindingsAccessor()["valueUpdate"];
     var newValueUpdate = ['afterkeypress'];
-    
+
     if (oldValueUpdate) {
         if (typeof oldValueUpdate == "string") {
             oldValueUpdate = [oldValueUpdate];
@@ -20,5 +21,6 @@ valueBindingHandler.init = function (element, valueAccessor, allBindingsAccessor
 
     originalInit(element, valueAccessor, allBindingsAccessor);
 };
+})();
 
 /**************  // KNOCKOUT.JS    *****************************/
