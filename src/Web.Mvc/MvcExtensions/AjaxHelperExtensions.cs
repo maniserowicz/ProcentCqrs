4312186e1a636c59f06af3b08028d3e4feff1995
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2011-07-22 08:14:39 +0200

    ajaxform helper now takes dictionary of parameters to enable passing BEFORE action as well as SUCCESS callback

--- src/Web.Mvc/MvcExtensions/AjaxHelperExtensions.cs
+++ src/Web.Mvc/MvcExtensions/AjaxHelperExtensions.cs
@@ -1,17 +1,19 @@
-﻿using System.Web.Mvc.Ajax;
+﻿using System.Collections.Generic;
+using System.Web.Mvc.Ajax;
 using System.Web.Mvc.Html;
 
 namespace System.Web.Mvc
 {
     public static class AjaxHelperExtensions
     {
-        public static MvcForm BeginForm(this AjaxHelper @this, ActionResult result, string onSuccess = null)
+        public static MvcForm BeginForm(this AjaxHelper @this, ActionResult result, IDictionary<string, string> actions = null)
         {
             var options = new AjaxOptions
                 {
-                    OnSuccess = onSuccess,
+                    OnSuccess = actions.ValueOrDefault("success"),
                     OnFailure = "cqrs.ajax.onError",
                     HttpMethod = "POST",
+                    OnBegin = actions.ValueOrDefault("before"),
                 };
 
             return @this.BeginForm(result, options, null);
