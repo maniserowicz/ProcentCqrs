cddc2f72a4f298be1a4cc4df34b0ac129ba83062
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2011-07-25 11:36:45 +0200

    introduced common SharedAssemblyInfo.cs linked in all projects

---
+++ src/SharedAssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ProcentCQRS")]
+[assembly: AssemblyDescription("Playground for experiments with CQRS in ASP.NET MVC (... and some other useful stuff like knockout.js, rake etc...)")]
+[assembly: AssemblyCompany("Maciej Aniserowicz")]
+[assembly: AssemblyProduct("ProcentCQRS")]
+[assembly: AssemblyCopyright("Copyright (C) Maciej Aniserowicz")]
