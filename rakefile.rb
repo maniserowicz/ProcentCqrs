3eb570feda5f81e0b05eb54c8484e4e9cf1c5ef4
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2011-07-25 11:24:57 +0200

    defined tasks for cleaning and compiling solution

--- rakefile.rb
+++ rakefile.rb
@@ -1,9 +1,13 @@
 # required gems
 require 'rubygems'
 require 'albacore'
+require 'rake/clean'
+
+def log(msg)
+    puts "***", msg, "***"
+end
 
 # define build variables
-OUTPUT = 'build'
 CONFIGURATION = 'Release'
 SLN_FILE = 'src/ProcentCqrs.sln'
 
@@ -11,4 +15,21 @@ SLN_FILE = 'src/ProcentCqrs.sln'
 Albacore.configure do |config|
     config.log_level = :verbose
     config.msbuild.use :net4
+end
+
+# introduce custom build targets
+desc "Compiles solution and runs unit tests"
+task :default => [:clean, :build]
+
+# configuration for rake/clean task
+CLEAN.include (FileList["src/**/#{CONFIGURATION}"])
+
+# 'build' task
+desc "builds solution"
+msbuild :build do |msb|
+    log "starting build"
+    msb.properties :configuration => CONFIGURATION
+    msb.targets :Clean, :Rebuild
+    msb.solution = SLN_FILE
+    msb.verbosity = "q"
 end
