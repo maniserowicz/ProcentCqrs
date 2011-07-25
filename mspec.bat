5c19cc78dd02fee9e707d5069649af646cec8015
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2011-07-25 12:32:11 +0200

    removed batches running tests - use rake for this from now on

--- mspec.bat
+++
@@ -1,8 +0,0 @@
-set mspec_path=.\tools\Machine.Specifications-Release.0.4.115\mspec-clr4.exe
-set test_dir=.\src\Tests\bin\Debug\
-set test_dll_path=%test_dir%ProcentCqrs.Tests.dll
-set html_output_path=%test_dir%mspec-output.html
-
-%mspec_path% %test_dll_path% --html %html_output_path%
-
-start %html_output_path%
--- xunit.bat
+++
@@ -1,5 +0,0 @@
-set xunit_path=.\tools\xunit-1.8\xunit.console.clr4.exe
-set test_dir=.\src\Tests\bin\Debug\
-set test_dll_path=%test_dir%ProcentCqrs.Tests.dll
-
-%xunit_path% %test_dll_path%
