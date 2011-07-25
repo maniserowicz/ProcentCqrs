ce5b9703385bda2694f460ea01ae02dd5c947721
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2011-07-25 11:55:56 +0200

    added assemblyinfo generation to build file

--- rakefile.rb
+++ rakefile.rb
@@ -10,6 +10,7 @@ end
 # define build variables
 CONFIGURATION = 'Release'
 SLN_FILE = 'src/ProcentCqrs.sln'
+VERSION = '0.1.0.0'
 
 # set albacore configuration variables
 Albacore.configure do |config|
@@ -26,10 +27,22 @@ CLEAN.include (FileList["src/**/#{CONFIGURATION}"])
 
 # 'build' task
 desc "builds solution"
-msbuild :build do |msb|
+msbuild :build => :asminfo do |msb|
     log "starting build"
     msb.properties :configuration => CONFIGURATION
     msb.targets :Clean, :Rebuild
     msb.solution = SLN_FILE
     msb.verbosity = "q"
+end
+
+# 'asminfo' task
+desc "generates SharedAssemblyInfo.cs file"
+assemblyinfo :asminfo do |asm|
+    asm.version = VERSION
+    asm.company_name = "Maciej Aniserowicz"
+    asm.title = "Procent.CQRS"
+    asm.product_name = "Procent.CQRS"
+    asm.description = "Playground for experiments with CQRS in ASP.NET MVC (and some other useful stuff like knockout.js, rake etc...)"
+    asm.copyright = "Copyright (C) Maciej Aniserowicz"
+    asm.output_file = 'src/SharedAssemblyInfo.cs'
 end
--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -1,8 +1,9 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("ProcentCQRS")]
-[assembly: AssemblyDescription("Playground for experiments with CQRS in ASP.NET MVC (... and some other useful stuff like knockout.js, rake etc...)")]
+[assembly: AssemblyTitle("Procent.CQRS")]
+[assembly: AssemblyDescription("Playground for experiments with CQRS in ASP.NET MVC (and some other useful stuff like knockout.js, rake etc...)")]
 [assembly: AssemblyCompany("Maciej Aniserowicz")]
-[assembly: AssemblyProduct("ProcentCQRS")]
+[assembly: AssemblyProduct("Procent.CQRS")]
 [assembly: AssemblyCopyright("Copyright (C) Maciej Aniserowicz")]
+[assembly: AssemblyVersion("0.1.0.0")]
+
